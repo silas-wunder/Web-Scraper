2d873430010f8b78408c75c08c2679635a10eb9f
Silas Wunder <55813205+silas-wunder@users.noreply.github.com>
2020-11-29 10:20:23 -0700

    initial commit

---
+++ scraper.py
@@ -0,0 +1,8 @@
+import requests
+from requests import get
+from bs4 import BeautifulSoup
+import pandas as pd
+import numpy as np
+
+url3070 = "https://www.microcenter.com/search/search_results.aspx?Ntt=3070&Ntk=all&sortby=match&N=4294966998&myStore=false"
+url3080 = "https://www.microcenter.com/search/search_results.aspx?Ntt=3080&Ntk=all&sortby=match&N=4294966998&myStore=false"
