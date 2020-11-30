fadca0e05ac1b57e61f58e131ccc8e4c2e0d41c1
Silas Wunder <55813205+silas-wunder@users.noreply.github.com>
2020-11-29 18:35:12 -0700

    grabs price, name, and url properly

--- scraper.py
+++ scraper.py
@@ -9,7 +9,6 @@ options.add_argument("--window-size=1920,1200")
 
 # the search page to look at
 url = "https://www.microcenter.com/search/search_results.aspx?N=4294966937+4294808776+4294808774+4294808740&NTK=all&sortby=match&rpp=96"
-url2 = "https://www.microcenter.com/search/search_results.aspx?Ntk=all&sortby=match&N=4294945779+40&myStore=false"
 
 # actually grabs the page html
 driver = webdriver.Firefox(options=options)
@@ -24,13 +23,19 @@ cards_in_stock = []
 
 for card_listing in cards:
     stock_amount = card_listing.find('div', class_='stock').find('strong').text
-    if stock_amount != '\nSold Out' and stock_amount != '\nNot Carried In This Store':
+    if stock_amount == '\nSold Out' or stock_amount == '\nNot Carried In This Store':
         cards_in_stock.append(card_listing)
-        print(card_listing.find('div', class_='normal').find('a').text)
-        print(stock_amount)
 
-# gives some output if there are no cards in stock, for testing purposes
-if len(cards_in_stock) == 0:
-    print("No cards found in stock")
+if len(cards_in_stock) != 0:
+    cards_formatted = {}
+    for card in cards_in_stock:
+        card_price = card.find('div', class_='price').text
+        card_url = "microcenter.com" + card.find('div', class_='normal').find('a', href=True).get('href')
+        card_name = card.find('div', class_='normal').text
+        card_price = card_price.strip()
+        card_url = card_url.strip()
+        card_name = card_name.strip().title()
+        cards_formatted[card_name] = (card_price, card_url)
+    
 
 driver.quit()
